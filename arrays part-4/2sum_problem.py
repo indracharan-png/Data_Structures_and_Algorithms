b53cf95223bf1a4c12b16bf7f84bda9d5c32a729
Indra Charan <indracharan000@gmail.com>
2025-09-18 17:23:08 -0700

    leetcode-1 optimized sol

--- arrays part-4/2sum_problem.py
+++ arrays part-4/2sum_problem.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         size = len(nums)
@@ -8,27 +7,15 @@ class Solution:
         # Map all the elements to their respective indices
         index_map = {}
         for i in range(size):
-            if nums[i] in index_map:
-                index_map[nums[i]].append(i)
-            else:
-                index_map[nums[i]] = [i]
+            index_map[nums[i]] = i
         
         # Iterate over the array
         for i in range(size):
             initial_val = nums[i]
-            # Substract the current element value from target, which gives the deficit value 
+            # Substracting the current element value from target gives the deficit value that is required
             deficit_val = target - initial_val
-            # See if that deficit value exists in the dictionary
-            if deficit_val in index_map:
-                # Check if the deficit value is same as initial num value
-                if deficit_val == initial_val:
-                    # Make sure there are multiple copies of these deficit/initial value 
-                    if len(index_map[deficit_val]) > 1:
-                        return [index_map[deficit_val].pop(), index_map[deficit_val].pop()]
-                else:
-                    return [i, index_map[deficit_val].pop()]
+            # See if that deficit value exists in the dictionary and make sure deficit and initial are not at same position
+            if deficit_val in index_map and index_map[deficit_val] != i:
+                return [i, index_map[deficit_val]]
         
         return []
-
-
-        
